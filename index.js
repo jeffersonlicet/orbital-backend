c7cf144f22c35cfe570423e5a22f57633305d4ac
Jefferson Licet <jeffersonlicet@gmail.com>
2019-11-05 18:01:48 -0300

    Move db to file

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
 import express from 'express';
-import { Sequelize } from 'sequelize';
-import {
-  development,
-} from './config/config';
-
-const sequelize = new Sequelize(development.database, development.username, development.password, {
-  host: development.host,
-  port: development.port,
-  dialect: 'mysql',
-});
+import db from './db/db';
 
 const app = express();
 const PORT = process.env.PORT || 8080;
