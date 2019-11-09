ceee7e7e9a0877bfbca738c354e3e4ff7db2b8ed
Jefferson Licet <jeffersonlicet@gmail.com>
2019-11-08 22:15:25 -0300

    Require dotenv

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
+
 import express from 'express';
 import db from './db/db';
 
+require('dotenv').config();
+
 const app = express();
 const PORT = process.env.PORT || 8080;
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
 
 app.listen(PORT, () => {
   console.log(`Listening on port:${PORT}`);
