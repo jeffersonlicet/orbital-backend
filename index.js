c8a84a29459619acef8e222f5c2f5337945bb809
Jefferson Licet <jeffersonlicet@gmail.com>
2019-11-10 20:10:28 -0300

    Initializing controllers

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 
 import express from 'express';
+
 import userRoutes from './routes/user';
 import authRoutes from './routes/auth';
+import initializeControllers from './controllers';
 
-import db from './db/db';
-
-require('dotenv').config();
+const controllers = initializeControllers({ a: true });
 
 const app = express();
 const PORT = process.env.PORT || 8080;
-
-app.use('/user', userRoutes);
-app.use('/auth', authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Listening on port:${PORT}`);
