e626e0917a0635cddaf94058cdfe492f2c9453ce
Jefferson Licet <jeffersonlicet@gmail.com>
2019-11-10 20:37:47 -0300

    Fancy commit

--- index.js
+++ index.js
@@ -5,13 +5,14 @@ import initializeModels from './models';
 import initializeRoutes from './routes';
 import initializeControllers from './controllers';
 
+const PORT = process.env.PORT || 8080;
+
 const { models, sequelize } = initializeModels();
 const controllers = initializeControllers({ models, sequelize });
 
 const app = express();
-initializeRoutes({ ...controllers, app });
 
-const PORT = process.env.PORT || 8080;
+initializeRoutes({ ...controllers, app });
 
 app.listen(PORT, () => {
   console.log(`Listening on port:${PORT}`);
