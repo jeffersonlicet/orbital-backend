59cd10f46f1c9ee849b36ced4c4e447de4fd8afa
Jefferson Licet <jeffersonlicet@gmail.com>
2019-11-10 20:09:26 -0300

    Dynamic controller initializer

---
+++ helpers/initializer.js
@@ -0,0 +1,14 @@
+import fs from 'fs';
+import path from 'path';
+
+const initialize = (filename, dirname, fn) => {
+  const basename = path.basename(filename);
+
+  fs.readdirSync(dirname)
+    .filter((file) => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
+    .forEach((file) => {
+      fn(path.join(dirname, file));
+    });
+};
+
+export default initialize;
