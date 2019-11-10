94ae2613738ee895f283288e9740cc49d82af074
Jefferson Licet <jeffersonlicet@gmail.com>
2019-11-09 23:03:08 -0300

    Import user routes

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 
 import express from 'express';
+import userRoutes from './routes/user';
+
 import db from './db/db';
 
 require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 8080;
+
+app.use('/user', userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Listening on port:${PORT}`);
