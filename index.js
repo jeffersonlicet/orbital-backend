371de231615fdc7ffe56db5e82c451808faa3953
Jefferson Licet <jeffersonlicet@gmail.com>
2019-11-11 20:11:32 -0300

    Adding body parser json

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
 import express from 'express';
+import bodyParser from 'body-parser';
 
 import initializeModels from './models';
 import initializeRoutes from './routes';
@@ -11,8 +12,13 @@ const { models, sequelize } = initializeModels();
 const controllers = initializeControllers({ models, sequelize });
 
 const app = express();
+app.use(bodyParser.json());
 
-initializeRoutes({ ...controllers, app });
+initializeRoutes(app, { ...controllers });
+
+app.use((err, req, res) => {
+  res.status(400).json(err);
+});
 
 app.listen(PORT, () => {
   console.log(`Listening on port:${PORT}`);
