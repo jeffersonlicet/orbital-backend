f1a57c31fe536fd13cfda2439c8caa663cfa7fe3
Jefferson Licet <jeffersonlicet@gmail.com>
2019-11-15 21:43:20 -0300

    Adding login route with passport strategy

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,11 @@
 import express from 'express';
+import passport from 'passport';
+import jwt from 'jsonwebtoken';
+
 import validate from '../helpers/validation';
-import UserSchema from '../schemas/user';
+import UserCreationSchema from '../schemas/userCreation';
+import UserLoginSchema from '../schemas/userLogin';
+
 import errorHandler from './middlewares/errorHandler';
 
 const authRouter = ({
@@ -12,9 +17,28 @@ const authRouter = ({
     res.send('Auth base');
   }));
 
-  router.post('/signup', validate(UserSchema), errorHandler(async (req, res, next) => {
+  router.post('/signup', validate(UserCreationSchema), errorHandler(async (req, res) => {
     const user = await userController.create(req.body);
     res.send({ user });
+  }));
+
+  router.post('/login', validate(UserLoginSchema), errorHandler(async (req, res) => {
+    passport.authenticate('local', { session: false }, (err, user) => {
+      if (err || !user) {
+        res.status(400).json({
+          message: 'Something is not right',
+          user,
+        });
+      } else {
+        req.login(user, { session: false }, (loginError) => {
+          if (loginError) {
+            res.send(loginError);
+          }
+          const token = jwt.sign(user, process.env.JWT_SECRET);
+          return res.json({ user, token });
+        });
+      }
+    })(req, res);
   }));
 
 
