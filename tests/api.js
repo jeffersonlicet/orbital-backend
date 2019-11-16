c3be0cccb32fa3595b90d0ad4bc6746f022a3f04
Jefferson Licet <jeffersonlicet@gmail.com>
2019-11-16 14:13:46 -0300

    New get method for supertests

--- tests/api.js
+++ tests/api.js
@@ -2,6 +2,10 @@ import request from 'supertest';
 
 const PORT = process.env.PORT || 8080;
 const api = request(`http://localhost:${PORT}`);
-const { post } = api;
+const { post, get } = api;
 api.post = (endpoint, data) => post(endpoint).send(data).set('Accept', 'application/json');
+api.get = (endpoint, token) => get(endpoint)
+  .set('Accept', 'application/json')
+  .set('Authorization', `Bearer ${token}`);
+
 export default api;
