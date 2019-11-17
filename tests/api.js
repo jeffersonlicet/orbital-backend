d091371793b3b9e0233825ad66bd3bb14fdc5e62
Jefferson Licet <jeffersonlicet@gmail.com>
2019-11-17 18:26:13 -0300

    Updating api POST to use token

--- tests/api.js
+++ tests/api.js
@@ -3,7 +3,18 @@ import request from 'supertest';
 const PORT = process.env.PORT || 8080;
 const api = request(`http://localhost:${PORT}`);
 const { post, get, put } = api;
-api.post = (endpoint, data) => post(endpoint).send(data).set('Accept', 'application/json');
+api.post = (endpoint, data, token) => {
+  const method = post(endpoint)
+    .send(data)
+    .set('Accept', 'application/json');
+
+  if (token) {
+    method.set('Authorization', `Bearer ${token}`);
+  }
+
+  return method;
+};
+
 api.get = (endpoint, token) => get(endpoint)
   .set('Accept', 'application/json')
   .set('Authorization', `Bearer ${token}`);
