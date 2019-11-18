00009f14a6ca5b6663d0cb9f6b0d7b9c92131dde
Jefferson Licet <jeffersonlicet@gmail.com>
2019-11-18 19:47:06 -0300

    Create, accept and decline invitations

---
+++ controllers/invitations.js
@@ -0,0 +1,89 @@
+/* eslint-disable class-methods-use-this */
+/* eslint-disable no-console */
+
+import BaseController from './BaseController';
+import { INVITATION_TYPES } from '../helpers/invitation';
+
+export default class InvitationsController extends BaseController {
+  async fetch(user, type, { offset, count }) {
+    const typesMap = {
+      sent: user.getInvitationsSent,
+      received: user.getInvitationsReceived,
+    };
+
+    try {
+      const method = typesMap[type];
+
+      if (!method) {
+        throw new Error('Invalid filter');
+      }
+
+      return await method.call(
+        user,
+        { limit: count, offset, through: { where: { status: 'pending' } } },
+      );
+    } catch (ex) {
+      console.log(ex);
+      throw new Error('Can\'t find friends user');
+    }
+  }
+
+  async create(user, id) {
+    try {
+      let invitation = await this.invitationModel.findOne({
+        where: { inviterId: user.id, inviteeId: id },
+      });
+
+      if (!invitation) {
+        invitation = await this.invitationModel.create({ inviterId: user.id, inviteeId: id });
+      }
+
+      return invitation;
+    } catch (ex) {
+      console.log(ex);
+      throw new Error('Can\'t find friends user');
+    }
+  }
+
+  async decline(id) {
+    try {
+      const invitation = this.invitationModel.findOne({ where: { id } });
+
+      if (invitation) {
+        await invitation.destroy();
+      }
+
+      return id;
+    } catch (ex) {
+      console.log(ex);
+      throw new Error('Can\'t find friends user');
+    }
+  }
+
+  async accept(user, id) {
+    try {
+      const invitation = await this.invitationModel.findOne({ where: { id, inviteeId: user.id } });
+      console.log(invitation);
+      if (invitation && invitation.status === INVITATION_TYPES.pending) {
+        await this.friendModel.create({
+          userId: invitation.inviterId,
+          friendId: invitation.inviteeId,
+        });
+
+        await this.friendModel.create({
+          userId: invitation.inviteeId,
+          friendId: invitation.inviterId,
+        });
+
+        await invitation.update({ status: INVITATION_TYPES.accepted });
+      } else {
+        throw new Error('invalid invitation');
+      }
+
+      return {};
+    } catch (ex) {
+      console.log(ex);
+      throw new Error('Can\'t find friends user');
+    }
+  }
+}
