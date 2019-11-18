96014d356f4d81d9f8fca1156de21617cfa53af2
Jefferson Licet <jeffersonlicet@gmail.com>
2019-11-18 19:47:18 -0300

    Adding raw user mock

--- tests/mock.js
+++ tests/mock.js
@@ -1,12 +1,45 @@
 import superagent from 'superagent';
+import api from './api';
 
-const user = async () => {
+export const rawUser = async () => {
   const { body } = await superagent.get('https://randomuser.me/api/');
   const [u] = body.results;
   const { password } = u.login;
   return {
     ...u,
     password: password.length < 6 ? '123456' : password,
+  };
+};
+
+const user = async () => {
+  const { body } = await superagent.get('https://randomuser.me/api/');
+  const [u] = body.results;
+  const { password } = u.login;
+  const account = {
+    ...u,
+    password: password.length < 6 ? '123456' : password,
+  };
+
+  const res = await api.post('/auth/signup', {
+    email: account.email,
+    password: account.password,
+    birthday: account.dob.date,
+    lastname: account.name.last,
+    avatar: account.picture.large,
+    firstname: account.name.first,
+    instagram: `${account.name.first}`,
+  });
+
+  return {
+    email: account.email,
+    password: account.password,
+    birthday: account.dob.date,
+    lastname: account.name.last,
+    avatar: account.picture.large,
+    firstname: account.name.first,
+    instagram: `${account.name.first}`,
+    id: res.body.user.id,
+    token: res.body.token,
   };
 };
 
