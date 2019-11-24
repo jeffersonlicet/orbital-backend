296aa972976a008dfe6a3cdeca2295e9822f712e
Jefferson Licet <jeffersonlicet@gmail.com>
2019-11-24 13:43:31 -0300

    Updating auth tests

--- tests/auth.spec.js
+++ tests/auth.spec.js
@@ -5,62 +5,34 @@ import api from './api';
 describe('Auth', () => {
   describe('/signup', () => {
     it('should signup and return the jwt', async () => {
-      const {
-        name, email, dob, picture, password,
-      } = await rawUser();
+      const randomRawUser = await rawUser();
 
-      const { status, body } = await api.post('/auth/signup', {
-        email,
-        password,
-        birthday: dob.date,
-        lastname: name.last,
-        avatar: picture.large,
-        firstname: name.first,
-        instagram: `${name.first}`,
-      });
+      const { status, body } = await api.post('/auth/signup', randomRawUser);
 
       expect(status).equals(200);
-      expect(body.user.firstname).equals(name.first);
+      expect(body.user.firstname).equals(randomRawUser.firstname);
       expect(!!body.token).equal(true);
     });
 
     it('should fail if invalid email', async () => {
-      const {
-        name, dob, picture, password,
-      } = await rawUser();
+      const randomRawUser = await rawUser();
+
 
       const { status } = await api.post('/auth/signup', {
+        ...randomRawUser,
         email: '123',
-        password,
-        birthday: dob.date,
-        lastname: name.last,
-        avatar: picture.large,
-        firstname: name.first,
-        instagram: `${name.first}`,
       });
 
       expect(status).equals(422);
     });
 
     it('should fail if existing email', async () => {
-      const {
-        name, email, dob, picture, password,
-      } = await rawUser();
+      const randomRawUser = await rawUser();
 
-      const data = {
-        email,
-        password,
-        birthday: dob.date,
-        lastname: name.last,
-        avatar: picture.large,
-        firstname: name.first,
-        instagram: `${name.first}`,
-      };
-
-      const { status } = await api.post('/auth/signup', data);
+      const { status } = await api.post('/auth/signup', randomRawUser);
       expect(status).equals(200);
 
-      const res = await api.post('/auth/signup', data);
+      const res = await api.post('/auth/signup', randomRawUser);
       expect(res.status).equals(422);
     });
 
@@ -72,45 +44,31 @@ describe('Auth', () => {
 
   describe('/login', () => {
     it('should login', async () => {
-      const {
-        name, email, dob, picture, password,
-      } = await rawUser();
+      const randomRawUser = await rawUser();
 
-      const { status, body } = await api.post('/auth/signup', {
-        email,
-        password,
-        birthday: dob.date,
-        lastname: name.last,
-        avatar: picture.large,
-        firstname: name.first,
-        instagram: `${name.first}`,
-      });
+      const { status, body } = await api.post('/auth/signup', randomRawUser);
 
       expect(status).equals(200);
-      const res = await api.post('/auth/login', { email, password });
+      const res = await api.post('/auth/login', {
+        email: randomRawUser.email,
+        password: randomRawUser.password,
+      });
+
       expect(res.status).equals(200);
       expect(!!res.body.token).equal(true);
       expect(res.body.user.id).equal(body.user.id);
     });
 
     it('should return error if invalid credentials', async () => {
-      const {
-        name, email, dob, picture, password,
-      } = await rawUser();
-
-      const { status } = await api.post('/auth/signup', {
-        email,
-        password,
-        birthday: dob.date,
-        lastname: name.last,
-        avatar: picture.large,
-        firstname: name.first,
-        instagram: `${name.first}`,
-      });
+      const randomRawUser = await rawUser();
+      const { status } = await api.post('/auth/signup', randomRawUser);
 
       expect(status).equals(200);
 
-      const res = await api.post('/auth/login', { email, password: '1234567' });
+      const res = await api.post('/auth/login', {
+        email: randomRawUser.email,
+        password: '1234567',
+      });
       expect(res.status).equals(400);
     });
   });
