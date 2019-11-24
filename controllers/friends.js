9e5eae909171218c510a71c4f6873adfed83b23d
Jefferson Licet <jeffersonlicet@gmail.com>
2019-11-24 13:43:23 -0300

    Remove friends on controller

--- controllers/friends.js
+++ controllers/friends.js
@@ -1,22 +1,44 @@
+/* eslint-disable class-methods-use-this */
 
 import BaseController from './BaseController';
 
 export default class FriendsController extends BaseController {
-  async fetch({ id }) {
+  async fetch(user) {
     try {
-      const users = await this.userModel.findOne({
-        where: {
-          id,
-        },
-      });
-      const friends = await users.getFriends();
+      const friends = await user.getFriends();
       return friends.map((friend) => ({
-        ...friend,
-        friendShip: undefined,
+        ...friend.buildView(),
+        friendship: undefined,
       }));
     } catch (ex) {
       console.log(ex);
-      throw new Error(`Can't find friends user`);
+      throw new Error('Can\'t find friends user');
+    }
+  }
+
+  async remove(user, friendId) {
+    try {
+      const query = (id1, id2) => this.friendModel.findOne({
+        where: {
+          friendId: id1,
+          userId: id2,
+        },
+      });
+
+      const [friendship, inverse] = await Promise.all([
+        query(user.id, friendId),
+        query(friendId, user.id),
+      ]);
+
+      if (friendship && inverse) {
+        await Promise.all([
+          friendship.destroy(),
+          inverse.destroy(),
+        ]);
+      }
+    } catch (ex) {
+      console.log(ex);
+      throw new Error('Can\'t find friends user');
     }
   }
 }
