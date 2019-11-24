b5e33dd3dee2935019d802e9b589be3da0bc8928
Jefferson Licet <jeffersonlicet@gmail.com>
2019-11-23 22:43:04 -0300

    Updating mock function

--- tests/mock.js
+++ tests/mock.js
@@ -1,43 +1,35 @@
-import superagent from 'superagent';
+import times from 'lodash/times';
+import random from 'lodash/random';
+
 import api from './api';
 
-export const rawUser = async () => {
-  const { body } = await superagent.get('https://randomuser.me/api/');
-  const [u] = body.results;
-  const { password } = u.login;
+const randomString = (len = 20) => times(len, () => random(35).toString(36)).join('');
+
+export const rawUser = () => {
+  const [
+    email,
+    firstname,
+    lastname,
+    password,
+    instagram,
+  ] = times(5, () => randomString(15));
   return {
-    ...u,
-    password: password.length < 6 ? '123456' : password,
+    email: `${email}@gmail.com`,
+    firstname,
+    lastname,
+    password,
+    instagram,
+    birthday: new Date(),
+    avatar: 'https://i.imgur.com/fjxSiYi.jpg',
   };
 };
 
 const user = async () => {
-  const { body } = await superagent.get('https://randomuser.me/api/');
-  const [u] = body.results;
-  const { password } = u.login;
-  const account = {
-    ...u,
-    password: password.length < 6 ? '123456' : password,
-  };
-
-  const res = await api.post('/auth/signup', {
-    email: account.email,
-    password: account.password,
-    birthday: account.dob.date,
-    lastname: account.name.last,
-    avatar: account.picture.large,
-    firstname: account.name.first,
-    instagram: `${account.name.first}`,
-  });
-
+  const randomUser = rawUser();
+  const res = await api.post('/auth/signup', randomUser);
+  console.log(res.body);
   return {
-    email: account.email,
-    password: account.password,
-    birthday: account.dob.date,
-    lastname: account.name.last,
-    avatar: account.picture.large,
-    firstname: account.name.first,
-    instagram: `${account.name.first}`,
+    ...randomUser,
     id: res.body.user.id,
     token: res.body.token,
   };
